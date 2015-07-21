ee839a03f2e989b5192e5011eb743a7604831acf
小无路 <wangyunchen@163.com>
2015-07-21 08:17:49 +0800

    #选择弹层的开发

--- developer/static/widget/basePopupSelect/basePopupSelect.js
+++ developer/static/widget/basePopupSelect/basePopupSelect.js
@@ -1,18 +1,36 @@
 var o = {}
-var city_tpl = __inline("basePopupSelect.html");
+var tpl = __inline("basePopupSelect.html");
 //var dd_cityList = $("#popupSelect");
 $(document).ready(function(){
- 		$("body").append(city_tpl);
+ 		$("body").append(tpl);
  		//dd_cityList = $("#dd_cityList");
  	init();
 });
 
+/*
+	data:{
+			isHide:false,
+			type:'radio',
+			selected:"12q",//radio的时候
+			group:[
+			{name:"北京大区",id:123},
+			{name:"沈阳地区",id:123}
+			],
+			showGroup:"武汉大区",
+			showList:[
+			{title:"A",item:[{name:"鞍山市",value:"12q",selected:false},{name:"鞍山市1",value:"112a",selected:true}]}
+			]
+	}
+
+*/
 
 function init(){
-		var city = new Vue({
+		var popupSelect_vue = new Vue({
 		el:"#dd-basePopupSelect",
 		data:{
 			isHide:false,
+			selected:"12q",
+			type:'radio',
 			group:[
 			{name:"北京大区",id:123},
 			{name:"上海大区",id:123},
@@ -22,10 +40,10 @@ function init(){
 			],
 			showGroup:"武汉大区",
 			showList:[
-			{title:"A",item:[{name:"鞍山市",value:"12"},{name:"鞍山市1",value:"112"}]},
-			{title:"B",item:[{name:"鞍山市",value:"12"},{name:"鞍山市1",value:"112"}]},
-			{title:"C",item:[{name:"鞍山市",value:"12"},{name:"鞍山市1",value:"112"},{name:"鞍山市",value:"12"},{name:"鞍山市1",value:"112"},{name:"鞍山市",value:"12"},{name:"鞍山市1",value:"112"},{name:"鞍山市",value:"12"},{name:"鞍山市1",value:"112"}]},
-			{title:"D",item:[{name:"鞍山市",value:"12"},{name:"鞍山市1",value:"112"}]}
+			{title:"A",item:[{name:"鞍山市",value:"12q"},{name:"鞍山市1",value:"112a"}]},
+			{title:"B",item:[{name:"鞍山市",value:"12s"},{name:"鞍山市1",value:"11s2"}]},
+			{title:"C",item:[{name:"鞍山市",value:"12x"},{name:"鞍山市1",value:"112c"},{name:"鞍山市",value:"12f"},{name:"鞍山市1",value:"1g2"},{name:"鞍山市",value:"12h"},{name:"鞍山市y1",value:"112o"},{name:"鞍山市",value:"1i2"},{name:"鞍山市1",value:"1m12"}]},
+			{title:"D",item:[{name:"鞍山市",value:"12c"},{name:"鞍山市1",value:"112v"}]}
 			]
 	},
 	"methods":{
@@ -39,23 +57,32 @@ function init(){
 		itemBoxClick:function(event){
 			var ele = event.target;
 			//input点击
-			this.isHide=true;
+			var self = this;
+			setTimeout(function(){
+				self.isHide=true;
+			},300);//延时一下让用户有视觉反应
 		},
 		okClick:function(){
 			//确认按钮点击
 			this.isHide=true;
 		},
 		cancelClick:function(){
-			//去掉按钮的点击
+			//取消按钮的点击
 			this.isHide=true;
 		}
 	},
 	"watch":{
 		"showGroup":function(val){
 				this.showList=groupData[val]||[];
-		}
+		},
+		"isHide":function(val){
+			if(!val){
+				this.data={};
+			}
+		}	
 	}
 });
+ return popupSelect_vue;
 }
 var groupData ={
 	"武汉大区":[
--- developer/view/layout/footer.php
+++ developer/view/layout/footer.php
@@ -1,6 +1,4 @@
  <script type="text/javascript" charset="utf-8" src="/static/libs/My97/WdatePicker.js"></script>
 <!--SCRIPT_PLACEHOLDER-->
 <!--RESOURCEMAP_PLACEHOLDER-->
-   <script>
-    require("history");
-   </script>
+   
